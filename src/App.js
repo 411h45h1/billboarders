b69533c829a02708e4d7b3fb4018e62c48817786
Ahmed <ahmedali.96@hotmail.com>
2020-08-14 18:39:35 -0400

    Revert "Updates"
    This reverts commit 90d72a340c181e25decb7d1afab380d3c7d3187a.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,39 @@ import AppState from "./context/AppState";
 import { Input } from "./components/Input";
 import { Output } from "./components/Output/";
 import { Scrollbars } from "react-custom-scrollbars";
+import { Grid, Button } from "semantic-ui-react";
 
 const App = () => {
   return (
     <AppState>
       <Scrollbars style={{ width: "100vw", height: "100vh" }}>
         <div className="App">
-          <header className="Header">
-            <p id="headerText">BillBoarders</p>
-          </header>
+          <Grid className="Header" columns="equal" id="Drag">
+            <Grid.Row id="Drag">
+              <Grid.Column>
+                <p id="headerText">BillBoarders</p>
+              </Grid.Column>
+
+              <Grid.Column>
+                <Button.Group id="NoDrag" floated="right">
+                  <Button
+                    as={"a"}
+                    href="https://github.com/AhmedAlihashi/billboarders"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    content="Click here for the repository"
+                  />
+                  <Button
+                    icon="power off"
+                    color="red"
+                    onClick={() => {
+                      window.close();
+                    }}
+                  />
+                </Button.Group>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
           <div className="Body">
             <Input />
             <Output />
