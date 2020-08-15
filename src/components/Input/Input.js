41c54da84ea0f6df14f7a7ec8602bbecdb1c28ef
Ahmed <ahmedali.96@hotmail.com>
2020-08-14 21:48:54 -0400

    responsive ui updates

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -7,6 +7,19 @@ import {
   Button,
 } from "semantic-ui-react";
 import AppContext from "../../context/appContext";
+import { createMedia } from "@artsy/fresnel";
+
+const AppMedia = createMedia({
+  breakpoints: {
+    mobile: 320,
+    tablet: 768,
+    computer: 992,
+    largeScreen: 1400,
+    widescreen: 1920,
+  },
+});
+
+const { Media } = AppMedia;
 
 const Input = () => {
   const appState = useContext(AppContext);
@@ -29,22 +42,75 @@ const Input = () => {
   };
   return (
     <Segment>
-      <Grid columns="equal">
+      <Grid centered>
         {/* Nav */}
         <Grid.Column width={16}>
           <Header as="div" block>
-            <Grid columns="equal">
-              <Grid.Column>
+            <Grid>
+              {/* Mobile */}
+              <Grid.Column floated="left" width={8} as={Media} at="mobile">
                 <Button
                   toggle
+                  size="huge"
                   active={firstButton}
                   onClick={firstButtonClick}
                   content="Billboard Top 200 Albums "
                 />
               </Grid.Column>
-              <Grid.Column>
+              <Grid.Column floated="right" width={7} as={Media} at="mobile">
                 <Button
                   toggle
+                  size="huge"
+                  active={secondButton}
+                  onClick={secondButtonClick}
+                  content="Billboard Hot 100"
+                />
+              </Grid.Column>
+
+              {/* Tablet */}
+              <Grid.Column floated="left" width={6} as={Media} at="tablet">
+                <Button
+                  toggle
+                  size="massive"
+                  active={firstButton}
+                  onClick={firstButtonClick}
+                  content="Billboard Top 200 Albums "
+                />
+              </Grid.Column>
+              <Grid.Column floated="right" width={6} as={Media} at="tablet">
+                <Button
+                  toggle
+                  size="massive"
+                  active={secondButton}
+                  onClick={secondButtonClick}
+                  content="Billboard Hot 100"
+                />
+              </Grid.Column>
+
+              {/* Greater */}
+              <Grid.Column
+                floated="left"
+                width={5}
+                as={Media}
+                greaterThanOrEqual="computer"
+              >
+                <Button
+                  toggle
+                  size="massive"
+                  active={firstButton}
+                  onClick={firstButtonClick}
+                  content="Billboard Top 200 Albums "
+                />
+              </Grid.Column>
+              <Grid.Column
+                floated="right"
+                width={5}
+                as={Media}
+                greaterThanOrEqual="computer"
+              >
+                <Button
+                  toggle
+                  size="massive"
                   active={secondButton}
                   onClick={secondButtonClick}
                   content="Billboard Hot 100"
@@ -58,13 +124,16 @@ const Input = () => {
 
         {secondButton && (
           <>
-            <Grid.Column width={4}>
+            {/* Mobile */}
+            <Grid.Column width={16} as={Media} at="mobile">
               <Header size="huge" textAlign="center">
                 Billboard Hot 100 Search
               </Header>
             </Grid.Column>
-            <Grid.Column width={3}>
+
+            <Grid.Column width={16} as={Media} at="mobile">
               <SemanticInput
+                style={{ width: "100%" }}
                 type="text"
                 onChange={(e) => setSearch(e.target.value)}
                 size="large"
@@ -73,7 +142,7 @@ const Input = () => {
                 placeholder="YYYY-MM-DD"
               />
             </Grid.Column>
-            <Grid.Column width={1}>
+            <Grid.Column width={8} as={Media} at="mobile">
               {search.length > 9 ? (
                 <Button
                   content="Submit"
@@ -81,8 +150,43 @@ const Input = () => {
                   onClick={() => getChartWeek(search)}
                 />
               ) : null}
+
+              {search.length > 9 ? (
+                <Button
+                  content="Clear"
+                  color="red"
+                  onClick={() => clearWeek()}
+                />
+              ) : null}
             </Grid.Column>
-            <Grid.Column width={1}>
+
+            {/* Tablet */}
+            <Grid.Column width={4} as={Media} greaterThanOrEqual="tablet">
+              <Header size="huge" textAlign="center">
+                Billboard Hot 100 Search
+              </Header>
+            </Grid.Column>
+
+            <Grid.Column width={8} as={Media} greaterThanOrEqual="tablet">
+              <SemanticInput
+                style={{ width: "100%" }}
+                type="text"
+                onChange={(e) => setSearch(e.target.value)}
+                size="large"
+                disabled={false}
+                loading={false}
+                placeholder="YYYY-MM-DD"
+              />
+            </Grid.Column>
+            <Grid.Column width={4} as={Media} greaterThanOrEqual="tablet">
+              {search.length > 9 ? (
+                <Button
+                  content="Submit"
+                  primary
+                  onClick={() => getChartWeek(search)}
+                />
+              ) : null}
+
               {search.length > 9 ? (
                 <Button
                   content="Clear"
